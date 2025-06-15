18557a11196a5f07c3a79f37a05ce9dcd896add5
Puzzlers Tech <tech@puzzlers-labs.com>
2025-06-16 06:13:36 +0900

    Add notarize template for future

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,8 +1,9 @@
 // Notarization script for macOS
+// This will be added later in the future
 // This is a placeholder - for production, you would implement actual notarization
 
-exports.default = async function(context) {
-  console.log('⏭️  Skipping notarization (not configured)');
+exports.default = async function (context) {
+  console.log("⏭️  Skipping notarization (not configured)");
   // In production, you would add notarization logic here
   // See: https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
-}; 
+};
